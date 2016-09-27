2d7bcb48762494da897b32a0efc590761bbcc82a
Andrew Jarrett <aj@Andrews-MacBook-Air.local>
2016-09-26 20:15:08 -0600

    added cart model and new cart instance per user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var router = require('express').Router();
+var Cart = require('../models/cart');
 var User = require('../models/user');
+var async = require('async');
 var passport = require('passport');
 var passportConfig = require('../config/passport');
 
@@ -7,7 +9,6 @@ router.get('/login', function(req, res) {
     if (req.user) return res.redirect('/');
     res.render('accounts/login', { message: req.flash('loginMessage')});
 });
-
 router.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile',
     failureRedirect: '/login',
@@ -26,31 +27,44 @@ router.get('/signup', function(req, res, next) {
         errors: req.flash('errors') 
     });
 });
-
 router.post('/signup', function(req, res, next) {
-    var user = new User();
+    async.waterfall([
 
-    user.profile.name = req.body.name;
-    user.password = req.body.password;
-    user.email = req.body.email;
-    user.profile.picture = user.gravatar();
+        //create new User
+        function(callback) {
+            var user = new User();
+            user.profile.name = req.body.name;
+            user.password = req.body.password;
+            user.email = req.body.email;
+            user.profile.picture = user.gravatar();
 
-    User.findOne({ email: req.body.email }, function(err, existingUser) {
+            User.findOne({ email: req.body.email }, function(err, existingUser) {
+                if (existingUser) {
+                    req.flash('errors', 'An account with that email already exists');
+                    return res.redirect('/signup');
+                } else {
+                    user.save(function(err, user) {
+                        if (err) return next(err);
+                        callback(null, user);
+                    });
+                }
+            });
+        },
 
-        if (existingUser) {
-            req.flash('errors', 'Account with that email already exists');
-            return res.redirect('/signup');
-        } else {
-            user.save(function(err, user) {
+        //create new Cart
+        function(user) {
+            var cart = new Cart();
+            cart.owner = user._id;
+            cart.save(function(err) {
                 if (err) return next(err);
-
                 req.logIn(user, function(err) {
                     if (err) return next(err);
                     res.redirect('/profile');
                 });
             });
         }
-    });
+
+    ]);
 });
 
 router.get('/logout', function(req, res, next) {
